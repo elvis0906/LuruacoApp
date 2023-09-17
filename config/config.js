7c4c9a76e35f89ea74f07c161b281cf48575c378
elvis0906 <138017690+elvis0906@users.noreply.github.com>
2023-09-16 19:57:21 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password:process.env.PASSWORD,    
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
     ssl: {"rejectUnauthorized":true}
 
     //  host: 'localhost',

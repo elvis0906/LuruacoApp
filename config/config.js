1363b2133ccc65d9a220c155afa91f781087c5ac
elvis0906 <138017690+elvis0906@users.noreply.github.com>
2023-09-18 13:31:45 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,17 +3,12 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
 
-    host: 'aws.connect.psdb.cloud',
-    user: 'g4xvbm3tz3h6otjpp4tt',
-    password: 'pscale_pw_vgrC4PzD8qKNqx5wLvHiF96OR39X6YkabJkQRPa0ZHN',
-    database: 'bd-luruaco_app',
-  
-   ssl: {"rejectUnauthorized":true}
 
-    //  host: 'localhost',
-    // user: 'root',
-    // password: '123456',
-    // database: 'Arepa_Luruaco'
+    port:  process.env.PORT,
+    host:   process.env.HOST,
+    user:  process.env.USER,
+    password:  process.env.PASSWORD,
+    database:  process.env.DATABASE
 
     
 });

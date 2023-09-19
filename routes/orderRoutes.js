0425a26622008eec46f1d6936c3fbb55eb568d58
elvis0906 <138017690+elvis0906@users.noreply.github.com>
2023-09-19 05:12:43 -0500

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -12,7 +12,7 @@ module.exports = (app) => {
     app.get('/api/orders/findByStatus/:status',  passport.authenticate('jwt', { session: false }), OrdersController.findByStatus);
     app.get('/api/orders/findByDeliveryAndStatus/:id_delivery/:status',  passport.authenticate('jwt', { session: false }), OrdersController.findByDeliveryAndStatus);
     app.get('/api/orders/findByClientAndStatus/:id_client/:status',  passport.authenticate('jwt', { session: false }), OrdersController.findByClientAndStatus);
-    app.post('/api/orders/create',  passport.authenticate('jwt', { session: false }), OrdersController.create);
+    app.post('/api/orders/create',  OrdersController.create);
     app.put('/api/orders/updateToDispatched',  passport.authenticate('jwt', { session: false }), OrdersController.updateToDispatched);
     app.put('/api/orders/updateToOnTheWay',  passport.authenticate('jwt', { session: false }), OrdersController.updateToOnTheWay);
     app.put('/api/orders/updateToDelivered',  passport.authenticate('jwt', { session: false }), OrdersController.updateToDelivered);

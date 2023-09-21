e9d61107265ef4ffb161876fc8f0876464dfffa2
elvis0906 <138017690+elvis0906@users.noreply.github.com>
2023-09-21 12:55:57 -0500

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,7 +2,7 @@ const productsController = require('../controllers/productsController');
 const passport = require('passport');
 
 module.exports = (app, upload) => {
-    app.get('/api/products/getAll',productsController.getAll);
+    app.get('/api/products/getAll', passport.authenticate('jwt', {session: false}) ,productsController.getAll);
     app.post('/api/products/create', passport.authenticate('jwt', {session: false}) , upload.array('image', 1), productsController.create);
     app.put('/api/products/updateWithImage', passport.authenticate('jwt', {session: false}) , upload.array('image', 3), productsController.updateWithImage);
     app.put('/api/products/update', passport.authenticate('jwt', {session: false}) , productsController.update);
